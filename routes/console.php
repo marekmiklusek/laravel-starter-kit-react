650e6e705b96f6aee1b26211c1dc235f2ae99b14
Marek Miklušek <marek.miklusek@gmail.com>
2026-08-25 17:20:13 +0200

    chore: remove playground command

--- routes/console.php
+++ routes/console.php
@@ -2,12 +2,7 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('playground', function (): void {
-    // ...
-});
 
 Schedule::command('db-backup:run')
     ->dailyAt('02:00')
