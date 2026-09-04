9f829e63d974119a207d25720de67d866204ec80
Marek Miklušek <marek.miklusek@gmail.com>
2026-09-04 19:11:05 +0200

    test: add architecture tests

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('queue:work --stop-when-empty --timeout=120 --max-time=300')
+    ->everyMinute()
+    ->withoutOverlapping(10)
+    ->runInBackground();
+
+Schedule::call(function (): void {
+    DB::table('cache')->where('expiration', '<', now()->getTimestamp())->delete();
+    Artisan::call('queue:prune-failed', ['--hours' => 168]);
+})->daily()->name('prune-expired');
 
 Schedule::command('db-backup:run')
     ->dailyAt('01:00')
--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -2,12 +2,57 @@
 
 declare(strict_types=1);
 
+use Mockery\MockInterface;
+use Mockery\LegacyMockInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+
 arch()->preset()->php();
 arch()->preset()->strict();
 arch()->preset()->security();
+arch()->preset()->laravel();
 
 arch('controllers')
     ->expect('App\Http\Controllers')
     ->not->toBeUsed();
 
-//
+arch('commands')
+    ->expect('App\Console\Commands')
+    ->not->toBeUsed();
+
+arch('actions')
+    ->expect('App\Actions')
+    ->classes()
+    ->not->toHaveSuffix('Action');
+
+arch('actions have an execute method')
+    ->expect('App\Actions')
+    ->toHaveMethod('execute')
+    ->ignoring('App\Actions\Fortify');
+
+arch('actions expose no other public method')
+    ->expect('App\Actions')
+    ->not->toHavePublicMethodsBesides(['execute', '__construct'])
+    ->ignoring('App\Actions\Fortify');
+
+arch('actions are readonly')
+    ->expect('App\Actions')
+    ->toBeReadonly()
+    ->ignoring('App\Actions\Fortify');
+
+arch('queries')
+    ->expect('App\Queries')
+    ->toBeReadonly()
+    ->toHaveSuffix('Query')
+    ->toHaveMethod('get');
+
+arch('queries expose no other public method')
+    ->expect('App\Queries')
+    ->not->toHavePublicMethodsBesides(['get', '__construct']);
+
+arch('no mocking')
+    ->expect('App')
+    ->not->toUse([
+        LegacyMockInterface::class,
+        MockInterface::class,
+        MockObject::class,
+    ]);
