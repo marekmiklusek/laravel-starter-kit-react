c5c654ad815be6eaef7a194783ae5bad29d5252b
Marek Miklušek <marek.miklusek@gmail.com>
2026-09-17 16:12:12 +0200

    chore: prune job batches table

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('queue:work --stop-when-empty --timeout=120 --max-time=300')
@@ -13,8 +12,11 @@ Schedule::command('queue:work --stop-when-empty --timeout=120 --max-time=300')
 
 Schedule::call(function (): void {
     DB::table('cache')->where('expiration', '<', now()->getTimestamp())->delete();
-    Artisan::call('queue:prune-failed', ['--hours' => 168]);
-})->daily()->name('prune-expired');
+})->daily()->name('prune-expired-cache');
+
+Schedule::command('queue:prune-failed', ['--hours' => 168])->daily();
+
+Schedule::command('queue:prune-batches', ['--hours' => 168])->daily();
 
 Schedule::command('db-backup:run')
     ->dailyAt('01:00')
